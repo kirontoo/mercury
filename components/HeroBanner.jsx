3fd645fbe4c333cee78c2da894b57b119e919892
Amy Dang <kirontoo@gmail.com>
2023-01-30 11:53:02 -0800

    feat: build out banner with dynamic values

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client.js";
 
-const HeroBanner = () => {
+const HeroBanner = ({ banner }) => {
   return (
-    <div>HeroBanner</div>
-  )
-}
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{banner.smallText}</p>
+      </div>
+      <h3>{banner.midText}</h3>
+      <h1>{banner.largeText1}</h1>
+      <img
+        className="hero-banner-image"
+        src={urlFor(banner.image)}
+        alt={banner.product}
+      />
+      <div>
+        <Link href={`/product/${banner.product}`}>
+          <button type="button">{banner.buttonText}</button>
+        </Link>
+        <div className="desc">
+          <h5>Description</h5>
+          <p>{banner.desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
