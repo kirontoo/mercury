fd1b4c5adc8b3eca01686d5b0c7b83b16340b36e
Amy Dang <kirontoo@gmail.com>
2023-01-31 21:02:18 -0800

    feat: create layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="layout">
+      <Head>
+        <title>Mercury</title>
+        <header>
+          <Navbar></Navbar>
+        </header>
+      </Head>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Mercury Headphones</Link>
+      </p>
+      <button className="cart-icon">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
