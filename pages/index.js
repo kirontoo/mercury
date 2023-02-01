eceec28c5d1f698d17fa0850a9c94db066079ff5
Amy Dang <kirontoo@gmail.com>
2023-01-31 20:27:09 -0800

    feat: fetch products from api and display

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
-export default function Home() {
+import { HeroBanner, Product, FooterBanner } from "../components";
+import { client } from "../lib/client";
+
+export default function Home({products, bannerData}) {
   return (
     <>
+      <HeroBanner banner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div className="producst-container">
-        {["product1", "product2"].map((product) => product)}
+      <div className="products-container">
+        {products?.map((product) => <Product key={product.id} product={product}/>)}
       </div>
+      <FooterBanner banner={bannerData && bannerData[0]}/>
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return { props: { products, bannerData } };
+};
