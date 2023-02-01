da5bdcf81b14f01418d7ee8c9b202f50b27943d6
Amy Dang <kirontoo@gmail.com>
2023-01-31 20:29:54 -0800

    feat: build out footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer-container">
+      <p>2023 Mercury Headphones All rights reserved</p>
+      <p className="icons">
+        <AiFillInstagram></AiFillInstagram>
+        <AiOutlineTwitter></AiOutlineTwitter>
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
