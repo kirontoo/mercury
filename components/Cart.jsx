96565a632bd71fe42d6091b468366791ef79cb02
Amy Dang <kirontoo@gmail.com>
2023-02-03 12:57:52 -0800

    feat: cart sidebar ui

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { useRef } from "react";
+import Link from "next/link";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import toast from "react-hot-toast";
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/client";
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext();
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button className="cart-heading" onClick={() => setShowCart(false)}>
+          <AiOutlineLeft></AiOutlineLeft>
+          <span className="heading">Your cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
 
-export default Cart
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button onClick={() => setShowCart(false)} className="btn">
+                Continue shopping
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {cartItems.length > 0 &&
+            cartItems.map((item, i) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  alt=""
+                  className="cart-product-image"
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>${item.price}</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span className="minus" onClick="">
+                          <AiOutlineMinus></AiOutlineMinus>
+                        </span>
+                        <span className="num">0</span>
+                        <span className="plus" onClick="">
+                          <AiOutlinePlus></AiOutlinePlus>
+                        </span>
+                      </p>
+                    </div>
+                    <button className="remove-item" onClick="">
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
