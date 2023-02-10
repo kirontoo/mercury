85fe0a234d8d6d3cb9e928748d4f6a7f322c1c9a
Amy Dang <kirontoo@gmail.com>
2023-02-10 13:59:27 -0800

    refactor: converto to MUI components

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,30 +1,69 @@
-import React from "react";
-import Link from "next/link";
 import { urlFor } from "../lib/client.js";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Link from "next/link";
+import { blueGrey } from "@mui/material/colors";
 
-const HeroBanner = ({ banner }) => {
+const HeroBanner = ({
+  banner: { desc, smallText, midText, image, product, largeText1 },
+}) => {
   return (
-    <div className="hero-banner-container">
-      <div>
-        <p className="beats-solo">{banner.smallText}</p>
-      </div>
-      <h3>{banner.midText}</h3>
-      <h1>{banner.largeText1}</h1>
-      <img
-        className="hero-banner-image"
-        src={urlFor(banner.image)}
-        alt={banner.product}
-      />
-      <div>
-        <Link href={`/product/${banner.product}`}>
-          <button type="button">{banner.buttonText}</button>
-        </Link>
-        <div className="desc">
-          <h5>Description</h5>
-          <p>{banner.desc}</p>
-        </div>
-      </div>
-    </div>
+    <Stack
+      borderRadius="0.5em"
+      backgroundColor="grey.300"
+      paddingY="5em"
+      paddingX="3em"
+      marginY="4em"
+      position="relative"
+    >
+      <Typography component="p" variant="h6">
+        {smallText}
+      </Typography>
+      <Typography
+        component="h3"
+        variant="h3"
+        fontWeight="bold"
+        color="primary"
+        fontSize={{ xs: "2.5em", md: "3em" }}
+      >
+        {midText}
+      </Typography>
+      <Typography
+        component="h1"
+        variant="h1"
+        fontWeight="bold"
+        fontSize={{ xs: "3em", md: "8em" }}
+        letterSpacing="0.05em"
+      >
+        {largeText1}
+      </Typography>
+      <img className="hero-banner-image" src={urlFor(image)} alt={product} />
+      <Box>
+        <Button variant="contained" href="/" component={Link} color="primary">
+          Shop Now
+        </Button>
+
+        <Stack
+          alignItems="flex-end"
+          spacing={1}
+          marginTop={{ xs: "4em", md: "0" }}
+        >
+          <Typography
+            variant="subtitle1"
+            component="h5"
+            fontWeight="bold"
+            color={blueGrey[800]}
+          >
+            Description
+          </Typography>
+          <Typography variant="subtitle2" component="p" color="grey.600">
+            {desc}
+          </Typography>
+        </Stack>
+      </Box>
+    </Stack>
   );
 };
 
