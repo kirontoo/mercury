20cd024eb9533191ef0f5173d2c3ffdc3755049f
Amy Dang <kirontoo@gmail.com>
2023-02-10 15:22:11 -0800

    refactor: convert to MUI components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 
 import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { blueGrey } from "@mui/material/colors";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <p>2023 Mercury Headphones All rights reserved</p>
-      <p className="icons">
-        <AiFillInstagram></AiFillInstagram>
-        <AiOutlineTwitter></AiOutlineTwitter>
-      </p>
-    </div>
+    <Stack
+      spacing={2}
+      sx={{
+        padding: "3em 5em",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h6" component="small" color={blueGrey[800]}>
+        2023 Mercury Headphones All rights reserved
+      </Typography>
+      <Box>
+        <Link href="https://example.com">
+          <AiFillInstagram fontSize="2em"></AiFillInstagram>
+        </Link>
+        <Link href="https://example.com">
+          <AiOutlineTwitter fontSize="2em"></AiOutlineTwitter>
+        </Link>
+      </Box>
+    </Stack>
   );
 };
 
