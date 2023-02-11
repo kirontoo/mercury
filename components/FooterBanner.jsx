fdff1877d605ca2863c35bbe65c2c47367466615
Amy Dang <kirontoo@gmail.com>
2023-02-10 16:25:06 -0800

    refactor: convert to MUI components

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,29 +1,88 @@
-import React from 'react'
-import Link from 'next/link';
-import { urlFor } from '../lib/client';
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
+import { red } from "@mui/material/colors";
 
-const FooterBanner = ({banner: {image, saleTime, smallText, midText, desc, discount, largeText1, largeText2,  buttonText}}) => {
+const FooterBanner = ({
+  banner: {
+    image,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    discount,
+    largeText1,
+    largeText2,
+    buttonText,
+  },
+}) => {
   return (
-    <div className="footer-banner-container">
-      <div className="banner-desc">
-        <div className="left">
-          <p>{ discount }</p>
-          <h3>{ largeText1 }</h3>
-          <h3>{ largeText2 }</h3>
-          <p>{ saleTime }</p>
-        </div>
-        <div className="right">
-          <p>{smallText}</p>
-          <h3>{midText}</h3>
-          <p>{desc}</p>
-          <Link href={`/product/product`}>
-            <button type="button">{buttonText}</button>
-          </Link>
-        </div>
-        <img src={ urlFor(image) } className="footer-banner-image" alt="" />
-      </div>
-    </div>
+    <Stack
+      borderRadius="1em"
+      sx={{ backgroundColor: "#f02d34" }}
+      paddingY="6em"
+      paddingX="3em"
+      color="white"
+      position="relative"
+      direction={{ xs: "column", md: "row" }}
+      justifyContent="space-between"
+      marginTop="12em"
+    >
+      <Box>
+        <Typography variant="subtitle1" component="span">
+          {discount}
+        </Typography>
+        <Typography
+          component="h3"
+          fontSize={{ xs: "3em", md: "5em" }}
+          fontWeight="bold"
+          lineHeight={1}
+        >
+          {largeText1}
+        </Typography>
+        <Typography
+          lineHeight={1}
+          fontSize={{ xs: "3em", md: "5em" }}
+          component="h3"
+          fontWeight="bold"
+        >
+          {largeText2}
+        </Typography>
+        <Typography variant="subtitle1" component="span">
+          {saleTime}
+        </Typography>
+      </Box>
+      <Stack spacing={2}>
+        <Typography variant="subtitle1" component="span">
+          {smallText}
+        </Typography>
+        <Typography variant="h2" component="h3" fontWeight="bold">
+          {midText}
+        </Typography>
+        <Typography variant="subtitle1" component="span">
+          {desc}
+        </Typography>
+        <Button
+          component={Link}
+          href="/products"
+          variant="outlined"
+          sx={{
+            width: "max-content",
+            background: "white",
+            "&:hover": {
+              background: red[100],
+            },
+          }}
+        >
+          {buttonText}
+        </Button>
+      </Stack>
+      <img src={urlFor(image)} className="footer-banner-image" alt="" />
+    </Stack>
   );
-}
+};
 
-export default FooterBanner
+export default FooterBanner;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
 import { HeroBanner, Product, FooterBanner } from "../components";
 import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import { client } from "../lib/client";
+import { blueGrey } from "@mui/material/colors";
 
 export default function Home({ products, bannerData }) {
   return (
     <>
       <HeroBanner banner={bannerData.length && bannerData[0]} />
-      <div className="products-heading">
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
+
       <Stack
-        direction={{ xs: "column",  md: "row"}}
+        justifyContent="center"
+        alignItems="center"
+        sx={{ padding: "5em 0 2em 0" }}
+      >
+        <Typography
+          variant="h3"
+          component="h2"
+          sx={{ color: blueGrey[800], fontWeigth: "bold", textAlign: "center" }}
+        >
+          Best Selling Products
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          component="p"
+          sx={{ color: blueGrey[600] }}
+        >
+          Speakers of many variations
+        </Typography>
+      </Stack>
+      <Stack
+        direction={{ xs: "column", md: "row" }}
         spacing={2}
         component="ul"
         justifyContent="center"
